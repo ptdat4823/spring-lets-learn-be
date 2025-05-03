95af31f4904f413f3ad7c3d3b1cb7bbf60cd6bf9
ptdat4823 <phamtiendat4823@gmail.com>
2025-05-04 06:53:09 +0700

    fix counting mark in single assignment report

--- src/main/java/com/letslive/letslearnbackend/services/TopicService.java
+++ src/main/java/com/letslive/letslearnbackend/services/TopicService.java
@@ -407,10 +407,10 @@ public class TopicService {
         reportDTO.setStudents(studentsThatTookPartIn.stream().map(st -> UserMapper.mapToDTO(st.getStudent())).toList());
         reportDTO.setStudentMarks(studentInfoAndMarks);
 
-        reportDTO.setStudentWithMarkOver8(studentInfoAndMarks.stream().filter(info -> info.getMark() != null && info.getMark() >= 8.0).toList());
-        reportDTO.setStudentWithMarkOver5(studentInfoAndMarks.stream().filter(info -> info.getMark() != null && info.getMark() >= 5.0 && info.getMark() < 8.0).toList());
-        reportDTO.setStudentWithMarkOver2(studentInfoAndMarks.stream().filter(info -> info.getMark() != null && info.getMark() >= 2.0 && info.getMark() < 5.0).toList());
-        reportDTO.setStudentWithMarkOver0(studentInfoAndMarks.stream().filter(info -> info.getMark() != null && info.getMark() < 2.0).toList());
+        reportDTO.setStudentWithMarkOver8(studentInfoAndMarks.stream().filter(info -> info.getSubmitted() && info.getMark() != null && info.getMark() >= 8.0).toList());
+        reportDTO.setStudentWithMarkOver5(studentInfoAndMarks.stream().filter(info -> info.getSubmitted() && info.getMark() != null && info.getMark() >= 5.0 && info.getMark() < 8.0).toList());
+        reportDTO.setStudentWithMarkOver2(studentInfoAndMarks.stream().filter(info -> info.getSubmitted() && info.getMark() != null && info.getMark() >= 2.0 && info.getMark() < 5.0).toList());
+        reportDTO.setStudentWithMarkOver0(studentInfoAndMarks.stream().filter(info -> info.getSubmitted() && info.getMark() != null && info.getMark() < 2.0).toList());
         reportDTO.setStudentWithNoResponse(studentInfoAndMarks.stream().filter(info -> !info.getSubmitted()).toList());
 
         reportDTO.setMarkDistributionCount(calculateMarkDistribution(studentWithMarkBase10, studentCount));
